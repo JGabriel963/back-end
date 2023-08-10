94ff11a628b05cf3764ca14a9ead944b41a72d0c
João Gabriel <joaogabriel9633@gmail.com>
2023-08-10 15:52:20 -0300

    Fim do Módulo 1

--- primeira_api/index.js
+++ primeira_api/index.js
@@ -2,20 +2,71 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?nome=NodeJS
 // Route Params = /curso/2
 // Request Body = { nome: "Nodejs", tipo: "Back-end" }
 
-server.get("/items", (req,res) => {
-    const nome = req.query.nome
+// CRUD -> Create, Read, Update, Delete
 
-    return res.json({  "message": `Item ${nome}`})
+const cursos = ["Node JS", "JavaScript", "React Native"];
+
+server.use((req, res, next) => {
+    console.log(`URL CHAMADA: ${req.url}`)
+
+    return next();
 })
 
-server.get("/curso/:id", (req, res) => {
-    const id = req.params.id;
+function checkCurso(req, res, next) {
+    if(!req.body.name) {
+        return res.status(400).json({ error : "Nome do curso é obrigatório"})
+    }
 
-    return res.json({ "curso": `Curso: ${id}` })
+    return next();
+}
+
+function checkIndexCurso(req, res, next) {
+    const curso = cursos[req.params.index]
+
+    if(!curso) {
+        return res.status(404).json({ error : "Curso não encotrado"})
+    }
+
+    return next();
+}
+
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+
+server.get("/cursos/:index", checkIndexCurso, (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
+});
+
+server.post("/cursos", checkCurso, (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+server.put("/cursos/:index", checkIndexCurso, checkCurso, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos)
+});
+
+server.delete('/cursos/:index', checkIndexCurso, (req, res) => {
+    const { index } = req.params;
+    
+    cursos.splice(index, 1);
+    return res.json(cursos)
 })
 
-server.listen(3000)
+server.listen(3000);
