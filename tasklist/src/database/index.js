38657a1975b757ef72a172de9415a08fd6deb92d
João Gabriel <joaogabriel9633@gmail.com>
2023-08-25 16:35:03 -0300

    Model Task

--- tasklist/src/database/index.js
+++ tasklist/src/database/index.js
@@ -1,19 +1,26 @@
-import  Sequelize  from "sequelize";
-import databaseConfig from "../config/database"
-import User from "../app/models/User";
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
 
-const modles = [User]
+import User from '../app/models/User';
+import Task from '../app/models/Task';
 
-class Datebase {
+const models = [User, Task];
+
+class Database {
     constructor() {
-        this.init()
+        this.init();
     }
 
     init() {
         this.connection = new Sequelize(databaseConfig);
 
-        modles.map(model => model.init(this.connection))
+        models
+            .map((model) => model.init(this.connection))
+            .map(
+                (model) =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
-export default new Datebase()
+export default new Database();
