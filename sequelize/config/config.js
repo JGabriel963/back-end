fb33a823b51af83b1fca77850b340ccfb3433b32
João Gabriel <joaogabriel9633@gmail.com>
2023-08-31 21:37:42 -0300

    Aprendendo mais sobre o Sequelize

---
+++ sequelize/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '130302jg',
+    database: 'curso_sequelize',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
---
+++ sequelize/migrations/20230901000922-planets.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("planets", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primayKey: true,
+      },
+      name: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      position: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("planets");
+  },
+};
